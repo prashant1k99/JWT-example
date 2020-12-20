a3c3b8731159b8d2e06f78606ac4ebf0cb4172fa
Prashant Singh <41435180+prashant1k99@users.noreply.github.com>
2020-12-20 21:11:36 +0530

    Posts => Added post methods

--- data/posts.js
+++ data/posts.js
@@ -1,13 +1,16 @@
 const posts = [
   {
+    id: 1,
     user: "prashant1k99",
     title: "Some interesting post 1."
   },
   {
+    id: 2,
     user: "prashant1k9",
     title: "Some interesting post 2."
   },
   {
+    id: 3,
     user: "prashant1k99",
     title: "Some interesting post 3."
   }
--- routes/posts/index.js
+++ routes/posts/index.js
@@ -11,4 +11,34 @@ router.get('/', validateAuthToken, (req, res) => {
   res.status(200).json(posts.filter(post => post.user === req.user.username))
 })
 
+router.post('/', validateAuthToken, (req, res) => {
+  try {
+    if (!req.body.title) res.status(422).send('Title missing.')
+    const lastElOfPost = posts[posts.length - 1]
+    posts.push({
+      id: lastElOfPost.id + 1,
+      user: req.user.username,
+      title: req.body.title
+    })
+    res.status(200).send('Success')
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.delete('/', validateAuthToken, (req, res) => {
+  try {
+    if (!req.body.id) res.status(422).send('Post id missing.')
+    posts.map((post, i) => {
+      if (post.user === req.user.username && post.id === req.body.id) {
+        posts.splice(i, 1)
+        return
+      }
+    })
+    res.status(201).send('Success')
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
 module.exports = router
